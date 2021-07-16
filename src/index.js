c1a1d24a3d0d8d698f034eb400e939ac7629a899
Marcus <marcuslt95@gmail.com>
2021-07-16 22:12:57 +0100

    Wrote js module for generating the html for the quick add form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './cssreset.css';
 import './style.css';
-const quickAddBtn = document.querySelector(".add-icon");
 
 const DATACONTROL = (function() {
     const projects =  [
@@ -21,4 +20,90 @@ const DATACONTROL = (function() {
         }
     }
     return {createNewProject, createNewToDo};
+})();
+
+const QuickAddForm = (function() {
+    const quickAddBtn = document.querySelector(".add-icon");
+    const createForm = function() {
+        const form = document.createElement("form");
+        form.classList.add("quickAddGrid");
+
+        const titleLabel = document.createElement("label");
+        titleLabel.setAttribute("for", "title");
+        titleLabel.setAttribute("id", "title-l");
+        titleLabel.textContent = "Title";
+
+        const titleInput = document.createElement("input");
+        titleInput.setAttribute("type", "text");
+        titleInput.setAttribute("id", "title");
+        titleInput.setAttribute("name", "title");
+
+        const descriptionLabel = document.createElement("label");
+        descriptionLabel.setAttribute("for", "description");
+        descriptionLabel.setAttribute("id", "description-l");
+        descriptionLabel.textContent = "Description";
+
+        const descriptionInput = document.createElement("textarea");
+        descriptionInput.setAttribute("id", "description");
+        descriptionInput.setAttribute("name", "description");
+
+        const projectLabel = document.createElement("label");
+        projectLabel.setAttribute("for", "project");
+        projectLabel.setAttribute("id", "project-l");
+
+        const projectRow = document.createElement("div");
+        projectRow.classList.add("project-row");
+
+        const select = document.createElement("select");
+        select.setAttribute("id", "project");
+        select.setAttribute("name", "project");
+        
+        const opt1 = document.createElement("option");
+        opt1.setAttribute("value", "");
+        opt1.setAttribute("selected", "true");
+        opt1.setAttribute("disabled", "true");
+        opt1.setAttribute("hidden", "true");
+        opt1.textContent = "Existing project";
+
+        const newProjectLabel = document.createElement("label");
+        newProjectLabel.setAttribute("for", "newProject");
+        newProjectLabel.style.display = "none";
+        newProjectLabel.textContent = "Or create a new project";
+
+        const newProjectInput = document.createElement("input");
+        newProjectInput.setAttribute("type", "text");
+        newProjectInput.setAttribute("name", "new-project");
+        newProjectInput.setAttribute("id", "new-project");
+        newProjectInput.setAttribute("placeholder", "or create a new project");
+
+        const flexHorizontal = document.createElement("div");
+        flexHorizontal.classList.add("flex-horizontal");
+
+        const cancelBtn = document.createElement("button");
+        cancelBtn.classList.add("cancel-btn");
+        cancelBtn.textContent = "Cancel";
+
+        const addBtn = document.createElement("button");
+        addBtn.classList.add("add-btn");
+        addBtn.textContent = "Add";
+
+        form.appendChild(titleLabel);
+        form.appendChild(titleInput);
+        form.appendChild(descriptionLabel);
+        form.appendChild(descriptionInput);
+        form.appendChild(projectLabel);
+
+        select.appendChild(opt1);
+        projectRow.appendChild(select);
+        projectRow.appendChild(newProjectLabel);
+        projectRow.appendChild(newProjectInput);
+
+        form.appendChild(projectRow);
+
+        flexHorizontal.appendChild(cancelBtn);
+        flexHorizontal.appendChild(addBtn);
+
+        form.appendChild(flexHorizontal);
+    }
+    return {createForm};
 })();
